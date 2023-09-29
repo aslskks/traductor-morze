ea6f19934a409cd9a8fc040f1780cb6fccc37e6a
aslskks <130728002+aslskks@users.noreply.github.com>
2023-09-28 19:34:06 -0600

    texto a morze

--- morze.py
+++ morze.py
@@ -1,27 +1,27 @@
-# Definimos un diccionario que mapea letras y números a su equivalente en código Morse
-codigo_morse = {
+# Definir el diccionario para mapear caracteres a código Morse
+morse_code_dict = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
     'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
     'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
-    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ' ': ' '
 }
 
-def texto_a_morse(texto):
-    texto = texto.upper()  # Convertimos el texto a mayúsculas para que funcione con letras minúsculas también
-    morse = ''
-    for caracter in texto:
-        if caracter == ' ':
-            morse += ' '  # Agregamos un espacio si encontramos un espacio en el texto
-        elif caracter in codigo_morse:
-            morse += codigo_morse[caracter] + ' '  # Agregamos el código Morse correspondiente y un espacio
+# Función para convertir texto a código Morse
+def text_to_morse(text):
+    text = text.upper()  # Convertir el texto a mayúsculas
+    morse_code = ""
+    for char in text:
+        if char in morse_code_dict:
+            morse_code += morse_code_dict[char] + " "  # Agregar un espacio entre cada letra
         else:
-            morse += caracter  # Mantenemos cualquier otro carácter sin cambios
-    return morse
+            morse_code += " "  # Agregar un espacio para los caracteres desconocidos
+    return morse_code
 
-def main():
-    texto = input("Ingresa el texto que deseas convertir a código Morse: ")
-    resultado = texto_a_morse(texto)
-    print("El texto en código Morse es:", resultado)
+# Ingresar el texto que deseas convertir
+texto = input("Ingresa el texto que deseas convertir a código Morse: ")
 
-if __name__ == "__main__":
-    main()
+# Llamar a la función y mostrar el resultado
+codigo_morse = text_to_morse(texto)
+print("Código Morse resultante: ", codigo_morse)
