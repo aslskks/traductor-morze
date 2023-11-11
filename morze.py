4a39e09ab61bcbed6a9739f34f436ae6ac5c3fc7
aslskks <130728002+aslskks@users.noreply.github.com>
2023-11-11 13:03:48 -0600

    code

--- morze.py
+++ morze.py
@@ -5,7 +5,7 @@ morse_code_dict = {
     '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z',
     '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
     ' ': ' ', '/': '-..-.', '?': '..--..', '!': '-.-.--', '@': '.--.-.', '&': '.-...', '(': '-.--.', ')': '-.--.-', '"': '.-..-.', "'": '.----.', ',': '--..--',
-    '$': '...-..-', '¡': '--...-', '¿': '..-.-', '+': '.-.-.', ':': '---...', '.': '.-.-.-', ';': '-.-.-.', '-':'-....-', '=': '-...-', '_': '..--.-',
+    '$': '...-..-', '¡': '--...-', '¿': '..-.-', '+': '.-.-.', ':': '---...', '.': '.-.-.-', ';': '-.-.-.', '-':'-....-', '=': '-...-', '_': '..--.-'
 }
 
 # Función para convertir texto a código Morse
@@ -19,9 +19,52 @@ def text_to_morse(text):
             morse_code += " "  # Agregar un espacio para los caracteres desconocidos
     return morse_code
 
-# Ingresar el texto que deseas convertir
-texto = input("Ingresa el texto que deseas convertir a código Morse: ")
+# Función para convertir código Morse a texto
+def morse_to_text(morse):
+    words = morse.split(' / ')  # Separamos el código Morse en palabras
+    text = ''
+    for word_morse in words:
+        letters_morse = word_morse.split(' ')  # Separamos cada palabra Morse en letras Morse
+        word_text = ''
+        for letter_morse in letters_morse:
+            if letter_morse in morse_code_dict:
+                word_text += morse_code_dict[letter_morse]  # Traducir cada letra Morse a texto
+            else:
+                word_text += '?'  # Si no reconocemos un símbolo Morse, lo representamos con '?'
+        text += word_text + ' '  # Agregamos la palabra traducida al texto y un espacio
+    return text.strip()  # Eliminamos espacios adicionales al principio y al final
 
-# Llamar a la función y mostrar el resultado
-codigo_morse = text_to_morse(texto)
-print("Código Morse resultante: ", codigo_morse)
+def main():
+    texto = input("Ingresa el texto que deseas convertir a código Morse: ").strip()
+    if not texto:
+        print("El texto ingresado está vacío.")
+        return
+
+    resultado = text_to_morse(texto)
+    print("El texto traducido a Morse es:", resultado)
+
+def main2():
+    morse = input("Ingresa el código Morse que deseas convertir a texto: ").strip()
+    if not morse:
+        print("El código Morse ingresado está vacío.")
+        return
+
+    resultado = morse_to_text(morse)
+    print("El código Morse traducido a texto es:", resultado)
+
+if __name__ == "__main__":
+    try:
+        print("¿Quieres ir de Morse a texto? (si o no) (exit para salir): ")
+        p = input("").lower()
+        if p == "si" or p == "yes" or p == "y" or p == "s":
+            main2()
+        elif p == "exit":
+            import sys
+            sys.exit()
+        else:
+            main()
+    except KeyboardInterrupt:
+        sys.exit()
+    except Exception as e:
+        from tkinter.messagebox import showerror
+        showerror(title="Morse", message=f"{e}")
